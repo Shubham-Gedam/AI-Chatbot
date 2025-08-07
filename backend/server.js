7cb9f0c09cf0d2ee3ec8ed113388b73ad558576c
Shubham-Gedam <shubhgedam28@gmail.com>
2025-08-07 09:10:32 +0530

    feat: initialize backend with Express and Socket.io
    - Add package.json with dependencies for Express, Socket.io, dotenv, and Google GenAI.
    - Create server.js to set up HTTP server and Socket.io connection.
    - Implement basic Express app in app.js with a Hello World route.
    - Add ai.service.js to handle AI response generation using Google GenAI.

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const app = require('./src/app');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const generateResponse = require('./src/service/ai.service');
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, );
+
+io.on("connection", (socket) => {
+      console.log("New client connected");
+
+    
+
+
+      //constomer event handling
+      socket.on("ai-message", async (data) => {
+        
+        console.log("Received AI message:", data.prompt);
+        const response = await generateResponse(data.prompt);
+        console.log("AI Response:", response);
+        socket.emit("ai-message-response", { response });
+        
+    })
+  
+});
+
+httpServer.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
